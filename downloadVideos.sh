990b7fd73ba761fa085286239cad041e574eed43
Bob van Luijt <bob@kubrickolo.gy>
2017-07-13 22:43:50 +0200

    Update downloadVideos.sh

--- downloadVideos.sh
+++ downloadVideos.sh
@@ -19,11 +19,6 @@
 # apt-get install unzip csvtool youtube-dl ffmpeg imagemagick
 #
 
-# Category to download (https://research.google.com/youtube8m/explore.html)
-CATDOWNLOAD=$(curl https://raw.githubusercontent.com/bobvanluijt/gan-movie/master/group.download) # <-- always change in git
-
-echo "Download all from category: $CATDOWNLOAD"
-
 ## Create and mount disk
 mkdir -p gan-project-results
 gcsfuse --implicit-dirs gan-project-results ./gan-project-results &>/dev/null
@@ -33,39 +28,46 @@ rm -f vocabulary.csv
 rm -f youtube-labels.csv
 wget --quiet https://github.com/bobvanluijt/gan-movie/blob/master/vocabulary.csv?raw=true -O vocabulary.csv
 wget --quiet https://github.com/bobvanluijt/gan-movie/blob/master/youtube-labels.csv.zip?raw=true -O youtube-labels.csv.zip
+wget --quite https://raw.githubusercontent.com/bobvanluijt/gan-movie/master/group.download -O group.download
 unzip youtube-labels.csv.zip &>/dev/null && rm -f youtube-labels.csv.zip && rm -rf __MACOSX/
 
 # shuffle to get random line per machine
 shuf -o youtube-labels.csv youtube-labels.csv
+shuf -o group.download group.download
 
 # Random sleep because of multiple machines running
 sleep $[ ( $RANDOM % 100 )  + 1 ]s
 
-# Get a random ID which is not downloaded yet
-while IFS='' read -r line || [[ -n "$line" ]]; do
-    # Check of the line has the wanted command
-    if echo "$line" | grep --quiet ,$CATDOWNLOAD,; then
-        # get the video id
-        VIDEOID=$(echo "$line" | csvtool -t ',' col "1" -)
-        # Check if the video isn't already being downloaded
-        if [ ! -f "./gan-project-results/vids/$VIDEOID.mp4" ]; then
-            # touch on the mp4 file so that other machines can find it too
-            touch "./gan-project-results/vids/$VIDEOID.mp4"
-            # Make dirs
-            mkdir -p ./gan-project-results/vids
-            mkdir -p ./gan-project-results/imgs
-            mkdir -p ./gan-project-results/imgs/$CATDOWNLOAD
-            # Download the video
-            youtube-dl --quiet --no-warnings --no-continue -f 'bestvideo[ext=mp4]/bestvideo' --merge-output-format mp4 -o "./gan-project-results/vids/${VIDEOID}.mp4" "${VIDEOID}" &>/dev/null
-            # Cut the screencaps
-            ffmpeg -nostdin -i "./gan-project-results/vids/${VIDEOID}.mp4" -vf "fps=1/15" -ss "15" -sseof "-15" "./gan-project-results/imgs/${CATDOWNLOAD}/${VIDEOID}-%03d.jpg" &>/dev/null
-            # Resize the caps
-            mogrify -resize 640x480 ./gan-project-results/imgs/${CATDOWNLOAD}/${VIDEOID}*
-            # Done
-            echo "DONE: $VIDEOID"
+# Loop through the ids in the groups
+while IFS='' read -r line || [[ -n "$CATDOWNLOAD" ]]; do
+    # Category to download (https://research.google.com/youtube8m/explore.html)
+    echo "Download all from category: $CATDOWNLOAD"
+    # Get a random ID which is not downloaded yet
+    while IFS='' read -r line || [[ -n "$line" ]]; do
+        # Check of the line has the wanted command
+        if echo "$line" | grep --quiet ,$CATDOWNLOAD,; then
+            # get the video id
+            VIDEOID=$(echo "$line" | csvtool -t ',' col "1" -)
+            # Check if the video isn't already being downloaded
+            if [ ! -f "./gan-project-results/vids/$VIDEOID.mp4" ]; then
+                # touch on the mp4 file so that other machines can find it too
+                touch "./gan-project-results/vids/$VIDEOID.mp4"
+                # Make dirs
+                mkdir -p ./gan-project-results/vids
+                mkdir -p ./gan-project-results/imgs
+                mkdir -p ./gan-project-results/imgs/$CATDOWNLOAD
+                # Download the video
+                youtube-dl --quiet --no-warnings --no-continue -f 'bestvideo[ext=mp4]/bestvideo' --merge-output-format mp4 -o "./gan-project-results/vids/${VIDEOID}.mp4" "${VIDEOID}" &>/dev/null
+                # Cut the screencaps
+                ffmpeg -nostdin -i "./gan-project-results/vids/${VIDEOID}.mp4" -vf "fps=1/15" -ss "15" -sseof "-15" "./gan-project-results/imgs/${CATDOWNLOAD}/${VIDEOID}-%03d.jpg" &>/dev/null
+                # Resize the caps
+                mogrify -resize 640x480 ./gan-project-results/imgs/${CATDOWNLOAD}/${VIDEOID}*
+                # Done
+                echo "DONE: $VIDEOID"
+            fi
         fi
-    fi
-done < "youtube-labels.csv"
+    done < "youtube-labels.csv"
+done < "group.download"
 
 echo "DONE DONE, prep for shutdown"
 
